6fff538a64f0ceb102777dfa116865cf8c403af3
rodrigo.paulino <rodrigo.paulino@prima.com.br>
2020-07-20 18:02:02 -0300

    Add forecast information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -15,7 +15,8 @@ const forecast = (latitude, longitude, callback) => {
             callback(undefined,
                 `${body.current.weather_descriptions[0]}. ` +
                 `It is currently ${body.current.temperature} degrees out. `+
-                `It feels like ${body.current.feelslike} degrees out.`)
+                `It feels like ${body.current.feelslike} degrees out. `+
+                `The humidity is ${body.current.humidity}%.`)
         }
     })
 }
